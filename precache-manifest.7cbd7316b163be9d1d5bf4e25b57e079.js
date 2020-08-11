d55c24d3b989502634e2de02e1a8b365b314a862
Sthefano Carvalho <sthefano@masteradin.com>
2020-08-11 12:41:20 -0300

    Updates

---
+++ precache-manifest.7cbd7316b163be9d1d5bf4e25b57e079.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1474f069758ccb56e0fe7c88a759f5",
+    "url": "/meditation-app/index.html"
+  },
+  {
+    "revision": "370645c75149a6482d9a",
+    "url": "/meditation-app/static/css/main.a76dfc45.chunk.css"
+  },
+  {
+    "revision": "d876afaab5a603b20b52",
+    "url": "/meditation-app/static/js/2.c3b40317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meditation-app/static/js/2.c3b40317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370645c75149a6482d9a",
+    "url": "/meditation-app/static/js/main.0b667e2b.chunk.js"
+  },
+  {
+    "revision": "b3e65cd34a71411ac468",
+    "url": "/meditation-app/static/js/runtime-main.4080ac6d.js"
+  },
+  {
+    "revision": "19878a7850c43a67a1c32c355ac3d23b",
+    "url": "/meditation-app/static/media/beach.19878a78.mp3"
+  },
+  {
+    "revision": "7e8a870bdf3e25a22222e41ae5d11d55",
+    "url": "/meditation-app/static/media/beach.7e8a870b.mp4"
+  },
+  {
+    "revision": "9e2bf4f1ed0f3523a816392b9598a211",
+    "url": "/meditation-app/static/media/beach.9e2bf4f1.svg"
+  },
+  {
+    "revision": "a87a0eacc49ffcd1737c7044ae5deee7",
+    "url": "/meditation-app/static/media/moving-outline.a87a0eac.svg"
+  },
+  {
+    "revision": "77ad076f0f909bffb3c7f061a61af702",
+    "url": "/meditation-app/static/media/pause.77ad076f.svg"
+  },
+  {
+    "revision": "cfc8963d92237a54a46a261d4823f747",
+    "url": "/meditation-app/static/media/play.cfc8963d.svg"
+  },
+  {
+    "revision": "4d060cd51aa2319980093e55b51e0fed",
+    "url": "/meditation-app/static/media/rain.4d060cd5.mp4"
+  },
+  {
+    "revision": "cd600007573f18e717944d21d44acc42",
+    "url": "/meditation-app/static/media/rain.cd600007.mp3"
+  },
+  {
+    "revision": "d7645cf4dcdd09ae31ba9a9d5edc6d13",
+    "url": "/meditation-app/static/media/rain.d7645cf4.svg"
+  },
+  {
+    "revision": "134a517b79e8745ffc91d7db1b9aa114",
+    "url": "/meditation-app/static/media/replay.134a517b.svg"
+  },
+  {
+    "revision": "91fbdf1d2d4180d47d795e9eafffa29a",
+    "url": "/meditation-app/static/media/track-outline.91fbdf1d.svg"
+  }
+]);
