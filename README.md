3275d13779cd09e4f78a501721ea447c9ee34635
Sthefano Carvalho <sthefano@masteradin.com>
2020-08-11 12:40:27 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
+# Meditation React App
+
+A medition website in which the user can choose for how long he will meditatate and what kind of background he wants (beach sounds or rain sounds).
+
+## Getting Started
+
+To get this running on another machine, just clone the repo and run the command to start the server:
+```
+npm start
+```
+
+
+### Prerequisites
+
+No external libraries were used. The media files for the project are inside the "assets" page, inside "src".
+
+
+
+### Styles
+
+The website is responsive. Using media-queries the app can be used on desktop or smaller screens.
+
+
+## Deployment
+
+The app is deployed on gh-pages. To make this work, it's necessary to install the npm package and add two scripts on "package.json" file.
+### Install gh-pages
+```
+npm install gh-pages --save-dev
+```
+### Scritps to add to package.json
+```
+"predeploy": "npm run build",
+"deploy": "gh-pages -d build"
+```
+
+## Built With
+
+* [Visual Studio Code](https://code.visualstudio.com/) - The editor
+* [Create React App](https://github.com/facebook/create-react-app) - The framework
+
+
+## Authors
+
+* **SthefanoC** - [SthefanoC](https://github.com/sthefanoc)
+
+
+## Acknowledgments
+
+* This project is the *nº02* commit of my [#100DaysOfCode](https://www.100daysofcode.com/) journey to improve my ReactJS abilities
+* The Meditation App is an adaptation of a [video](https://youtu.be/oMBXdZzYqEk) made by [DevEd](https://github.com/developedbyed)
+* Follow my projects on twitter: [Sthefano_C](https://twitter.com/Sthefano_C) :)
+
+
+
+
+
+
+
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,152 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import videoRain from './assets/video/rain.mp4';
+import videoBeach from './assets/video/beach.mp4';
 
-export default App;
+import audioRain from './assets/sounds/rain.mp3';
+import audioBeach from './assets/sounds/beach.mp3';
+
+import imgBeach from './assets/svg/beach.svg';
+import imgMovingOutline from './assets/svg/moving-outline.svg';
+import imgPause from './assets/svg/pause.svg';
+import imgPlay from './assets/svg/play.svg';
+import imgRain from './assets/svg/rain.svg';
+import imgReplay from './assets/svg/replay.svg';
+import imgTrackOutline from './assets/svg/track-outline.svg';
+
+export default class App extends Component {
+  componentDidMount(){
+    const song = document.querySelector(".song");
+    const play = document.querySelector(".play");
+    const pause = document.querySelector(".pause");
+    const replay = document.querySelector(".replay");
+    const outline = document.querySelector(".moving-outline circle");
+    const video = document.querySelector(".vid-container video");
+    //Sounds
+    const sounds = document.querySelectorAll(".sound-picker button");
+    //Time Display
+    const timeDisplay = document.querySelector(".time-display");
+    const outlineLength = outline.getTotalLength();
+    //Duration
+    const timeSelect = document.querySelectorAll(".time-select button");
+    let fakeDuration = 600;
+
+    outline.style.strokeDashoffset = outlineLength;
+    outline.style.strokeDasharray = outlineLength;
+    timeDisplay.textContent = `${Math.floor(fakeDuration / 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}:${Math.floor(
+      fakeDuration % 60
+    ).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}`;
+
+    sounds.forEach(sound => {
+      sound.addEventListener("click", function() {
+        song.src = this.getAttribute("data-sound");
+        video.src = this.getAttribute("data-video");
+        checkPlaying(song);
+      });
+    });
+
+    play.addEventListener("click", function() {
+      checkPlaying(song);
+    });
+
+    pause.addEventListener("click", function() {
+      checkPlaying(song);
+    });
+
+    replay.addEventListener("click", function() {
+        restartSong(song);
+        
+      });
+
+
+    const restartSong = song =>{
+        let currentTime = song.currentTime;
+        song.currentTime = 0;
+    }
+
+    timeSelect.forEach(option => {
+      option.addEventListener("click", function() {
+        fakeDuration = this.getAttribute("data-time");
+        timeDisplay.textContent = `${Math.floor(fakeDuration / 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}:${Math.floor(
+          fakeDuration % 60
+        ).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}`;
+      });
+    });
+
+    const checkPlaying = song => {
+      if (song.paused) {
+        song.play();
+        video.play();
+        // play.src = {imgPlay};
+        play.classList.add('hidden');
+        pause.classList.remove('hidden');
+      } else {
+        song.pause();
+        video.pause();
+        // play.src = {imgPause};
+        pause.classList.add('hidden');
+        play.classList.remove('hidden');
+      }
+    };
+
+    song.ontimeupdate = function() {
+      let currentTime = song.currentTime;
+      let elapsed = fakeDuration - currentTime;
+      let seconds = Math.floor(elapsed % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+      let minutes = Math.floor(elapsed / 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+      timeDisplay.textContent = `${minutes}:${seconds}`;
+      let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+      outline.style.strokeDashoffset = progress;
+
+      if (currentTime >= fakeDuration) {
+        song.pause();
+        song.currentTime = 0;
+        // play.src = {imgPlay};
+        pause.classList.add('hidden');
+        play.classList.remove('hidden');
+        video.pause();
+      }
+    };
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <h1 className="title">Meditation App</h1>
+        <div className="app">
+          <div className="vid-container">
+            <video loop >
+              <source src={videoRain}  type="video/mp4" />
+            </video>
+          </div>
+          <div className="time-select">
+            <button data-time="120">2 Minutes</button>
+            <button data-time="300" className="medium-mins">5 Minutes</button>
+            <button data-time="600" className="long-mins">10 Minutes</button>
+          </div>
+          <div className="player-container">
+            <audio className="song">
+              <source src={audioRain} />
+            </audio>
+            <img src={imgPlay} className="play"></img>
+            <img src={imgPause} className="pause hidden"></img>
+            <svg className="track-outline" width="453" height="453" viewBox="0 0 453 453" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="226.5" cy="226.5" r="216.5" stroke="white" stroke-width="20"/>
+              </svg>
+            <svg className="moving-outline" width="453" height="453" viewBox="0 0 453 453" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="226.5" cy="226.5" r="216.5" stroke="#018EBA" stroke-width="20"/>
+              </svg>
+            <img src={imgReplay} className="replay"></img>
+          
+              <h3 className="time-display">0:00</h3>      
+          </div>
+          <div className="sound-picker">
+            <button data-sound={audioRain} data-video={videoRain} ><img src={imgRain} alt="" /></button>
+            <button data-sound={audioBeach} data-video={videoBeach}><img src={imgBeach} alt="" /></button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
